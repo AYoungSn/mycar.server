fc9ff5b6be20d231c6ab9b5ee93359aeed46fd21
AYoungSn <dudtjs0920@naver.com>
2023-07-17 20:21:39 +0900

    [docs] car controller swagger api response 목록 추가

--- src/main/java/com/autoever/mycar/server/car/adapter/in/web/CarController.java
+++ src/main/java/com/autoever/mycar/server/car/adapter/in/web/CarController.java
@@ -9,7 +9,12 @@ import com.autoever.mycar.server.car.adapter.in.web.dto.res.TrimListResDto;
 import com.autoever.mycar.server.car.adapter.out.view.CarResDto;
 import com.autoever.mycar.server.car.application.service.CarService;
 import com.autoever.mycar.server.car.domain.code.CarCode;
+import com.autoever.mycar.server.car.exception.ModelNotFoundException;
+import com.autoever.mycar.server.global.exception.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,37 +32,61 @@ public class CarController {
     private final CarService carService;
 
     @GetMapping
-    @Operation(summary = "차 종류 목록 조회(메뉴 목록 렌더링을 위한 API)")
+    @Operation(summary = "차 종류 목록 조회(메뉴 목록 렌더링을 위한 API)", responses = {
+            @ApiResponse(responseCode = "200", description = "목록 조회 성공",
+                    content = @Content(schema = @Schema(implementation = List.class)))
+    })
     public List<CarResDto> getCarList() {
         return carService.getCarList();
     }
 
     @GetMapping("/model-filter")
-    @Operation(summary = "선택 가능한 툴팁 목록 조회")
+    @Operation(summary = "선택 가능한 툴팁 목록 조회", responses = {
+            @ApiResponse(responseCode = "200", description = "선택 가능한 툴팁 목록 조회 성공",
+                    content = @Content(schema = @Schema(implementation = ToolTipListDto.class))),
+            @ApiResponse(responseCode = "400", description = "carCode 값이 들어오지 않음",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     public ToolTipListDto getToolTips(@Valid @ParameterObject ToolTipReqDto reqDto) {
         return carService.findToolTips(reqDto);
     }
 
     @GetMapping("/trims")
-    @Operation(summary = "선택한 툴팁에 맞는 모델 리스트 조회")
+    @Operation(summary = "선택한 툴팁에 맞는 모델 리스트 조회", responses = {
+            @ApiResponse(responseCode = "200", description = "모델 리스트 조회 성공",
+                    content = @Content(schema = @Schema(implementation = TrimListResDto.class))),
+    })
     public TrimListResDto getTrims(@Valid @ParameterObject ModelFilterReqDto reqDto) {
         return new TrimListResDto(carService.findModelsByToolTips(reqDto));
     }
 
     @GetMapping("/models/{modelId}/details")
-    @Operation(summary = "내차만들기 페이지 초기 렌더링을 위한 API")
+    @Operation(summary = "내 차 만들기 페이지 초기 렌더링을 위한 API", responses = {
+            @ApiResponse(responseCode = "200", description = "차량 상세 정보 조회 성공",
+                    content = @Content(schema = @Schema(implementation = ModelDetailResDto.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 모델 id",
+                    content = @Content(schema
+                            = @Schema(implementation = ModelNotFoundException.class)))
+    })
     public ModelDetailResDto getModelDetails(@PathVariable Long modelId) {
         return carService.myCarInit(modelId);
     }
 
     @GetMapping("/{carCode}/models")
-    @Operation(summary = "트림 변경 모달에서 변경 가능한 모델 종류를 보여주기 위한 API")
+    @Operation(summary = "트림 변경 모달에서 변경 가능한 모델 종류를 보여주기 위한 API", responses = {
+            @ApiResponse(responseCode = "200", description = "모델 이름 조회 성공",
+                    content = @Content(schema
+                            = @Schema(implementation = ModelBasicInfoListDto.class)))
+    })
     public ModelBasicInfoListDto getModelList(@PathVariable CarCode carCode) {
         return carService.findModelBasicInfo(carCode);
     }
 
     @GetMapping("/{carCode}/trims")
-    @Operation(summary = "트림 변경 모달에서 모델 기본 이름 별로 선택 가능한 트림 목록 상세 조회")
+    @Operation(summary = "트림 변경 모달에서 모델 기본 이름 별로 선택 가능한 트림 목록 상세 조회", responses = {
+            @ApiResponse(responseCode = "200", description = "트림 목록 상세 조회",
+                    content = @Content(schema = @Schema(implementation = TrimListResDto.class)))
+    })
     public TrimListResDto getTrimListByModelName(
             @PathVariable CarCode carCode,
             String modelBasicName) {
